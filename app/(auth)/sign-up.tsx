3d936a76c23254b53e2db6996474d6d7d11eb032
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-09-04 10:38:37 +0100

    Creating an account on Neon(Postgres) & logging in

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -7,6 +7,7 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import ReactNativeModal from "react-native-modal";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
@@ -51,7 +52,14 @@ const SignUp = () => {
       });
 
       if (completeSignUp.status === "complete") {
-        // TODO: Create a database user
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          }),
+        });
 
         await setActive({ session: completeSignUp.createdSessionId });
         setVerification({
