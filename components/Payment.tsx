82cb3cdcf3769a4df856f75e643ab290ddfb4e75
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-09-19 19:22:13 +0100

    Starting with Stripe

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,6 +1,41 @@
-import { Text, View } from "react-native";
+import { useStripe } from "@stripe/stripe-react-native";
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import { PaymentProps } from "@/types/type";
 
-const Payment = () => {
+const Payment = ({ amount }: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  // const { userId } = useAuth();
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+    }
+  };
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: parseInt(amount) * 100,
+          currencyCode: "usd",
+        },
+        confirmHandler: async(),
+      },
+    });
+    if (!error) {
+      // setLoading(true);
+    }
+  };
   return (
     <>
       <CustomButton
