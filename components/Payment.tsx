7d557196c316fd89d217cc5eba8ba57dd0eb7d84
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-09-20 19:00:12 +0100

    Continuing with Stripe setup

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -4,7 +4,13 @@ import { Alert } from "react-native";
 import CustomButton from "@/components/CustomButton";
 import { PaymentProps } from "@/types/type";
 
-const Payment = ({ amount }: PaymentProps) => {
+const Payment = ({
+  fullName,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
   // const { userId } = useAuth();
   const [success, setSuccess] = useState<boolean>(false);
@@ -29,8 +35,69 @@ const Payment = ({ amount }: PaymentProps) => {
           amount: parseInt(amount) * 100,
           currencyCode: "usd",
         },
-        confirmHandler: async(),
+        confirmHandler: async (
+          paymentMethod,
+          shouldSavePaymentMethod,
+          intentCreationCallback,
+        ) => {
+          const { paymentIntent, customer } = await fetchAPI(
+            "/(api)/(stripe)/create",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name: fullName || email.split("@")[0],
+                email: email,
+                amount: amount,
+                paymentMethodId: paymentMethod.id,
+              }),
+            },
+          );
+          if (paymentIntent.client_secret) {
+            const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                payment_method_id: paymentMethod.id,
+                payment_intent_id: paymentIntent.id,
+                customer_id: customer,
+                client_secret: paymentIntent.client_secret,
+              }),
+            });
+
+            if (result.client_secret) {
+              await fetchAPI("/(api)/ride/create", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  origin_address: userAddress,
+                  destination_address: destinationAddress,
+                  origin_latitude: userLatitude,
+                  origin_longitude: userLongitude,
+                  destination_latitude: destinationLatitude,
+                  destination_longitude: destinationLongitude,
+                  ride_time: rideTime.toFixed(0),
+                  fare_price: parseInt(amount) * 100,
+                  payment_status: "paid",
+                  driver_id: driverId,
+                  user_id: userId,
+                }),
+              });
+
+              intentCreationCallback({
+                clientSecret: result.client_secret,
+              });
+            }
+          }
+        },
       },
+      returnURL: "myapp://book-ride",
     });
     if (!error) {
       // setLoading(true);
