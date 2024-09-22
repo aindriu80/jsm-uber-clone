d76330d8b3744b81917cb213b508fda0c87fa0d5
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-09-22 18:10:11 +0100

    Completing the payment page

--- components/Map.tsx
+++ components/Map.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 import { icons } from "@/constants";
 import { useFetch } from "@/lib/fetch";
 import { ActivityIndicator, Text, View } from "react-native";
+import MapViewDirections from "react-native-maps-directions";
 
 const Map = () => {
   const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
@@ -99,6 +100,29 @@ const Map = () => {
           }
         />
       ))}
+      {destinationLatitude && destinationLongitude && (
+        <>
+          <Marker
+            key="destination"
+            coordinate={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            title="Destination"
+            image={icons.pin}
+          />
+          <MapViewDirections
+            origin={{ latitude: userLatitude!, longitude: userLongitude! }}
+            destination={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            apiKey={process.env.EXPO_PUBLIC_GOOGLE_API_KEY}
+            strokeColor="#0286ff"
+            strokeWidth={2}
+          />
+        </>
+      )}
     </MapView>
   );
 };
